f35efb7f6a917931ffb24751cd6fa8e6ae8717cd
rey09 <reynaldo.johanes@yahoo.com>
2017-07-17 17:04:21 +0700

    db
    pertama kali database

---
+++ database/migrations/2017_07_17_094404_create_flows_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFlowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flows', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flows');
+    }
+}
